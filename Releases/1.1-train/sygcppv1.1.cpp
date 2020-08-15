78b1f26ec22df382b772ff131e188cd12c6fbcd9
acetone <acetone@disroot.org>
2020-08-15 18:21:32 +0000

    Some fixes by R4SAS

--- Releases/1.1-train/sygcppv1.1.cpp
+++ Releases/1.1-train/sygcppv1.1.cpp
@@ -16,13 +16,13 @@
 #include <iostream>      // вывод на экран
 #include <string>
 #include <sstream>
-#include <fstream>		 // файловые потоки
+#include <fstream>       // файловые потоки
 #include <iomanip>       // форматированный вывод строк
 #include <bitset>        // побитовое чтение
 #include <vector>
-#include <thread>		 // многопоточность
+#include <thread>        // многопоточность
 #include <mutex>
-#include <chrono>		 // для паузы в заставке
+#include <chrono>        // для паузы в заставке
 #include <ctime>
 
 ////////////////////////////////////////////////// Заставка
@@ -59,31 +59,31 @@ int conf_high = 100;
 std::string conf_search;
 std::string log_file;
 
-uint64_t totalcount = 0;	// счетчик основного цикла
+uint64_t totalcount = 0;        // счетчик основного цикла
 uint64_t totalcountfortune = 0; // счетчик нахождений
-bool newline = true;	// используется для вывода счетчика
+bool newline = true;            // используется для вывода счетчика
 
 int config()
 {
 	std::ifstream conffile ("sygcpp.conf");
 
-	if(!conffile)	// проверка наличия конфига
+	if(!conffile) // проверка наличия конфига
 	{
 		std::cout << " Configuration file not found..." << std::endl;
 		conffile.close();
-		std::ofstream newconf ("sygcpp.conf");	// создание конфига
+		std::ofstream newconf ("sygcpp.conf"); // создание конфига
 		if(!newconf)
 		{
 			std::cerr << " Config (sygcpp.conf) creation failed :(" << std::endl;
 			return -1;
 		}
 		newconf << "1 0 1 9 ::\n"
-				<< "| | | | ^Pattern for search by name.\n"
-				<< "| | | ^Start position for high addresses search.\n"
-				<< "| | ^Logging mode (0 - console output only, 1 - log to file).\n"
-				<< "| ^Mining mode (0 - by name, 1 - high address).\n"
-				<< "^Count of thread (mining streams).\n\n"
-				<< "Parameters are separated by spaces.";
+		        << "| | | | ^Pattern for search by name.\n"
+		        << "| | | ^Start position for high addresses search.\n"
+		        << "| | ^Logging mode (0 - console output only, 1 - log to file).\n"
+		        << "| ^Mining mode (0 - by name, 1 - high address).\n"
+		        << "^Count of thread (mining streams).\n\n"
+		        << "Parameters are separated by spaces.";
 		newconf.close();
 		std::ifstream conffile ("sygcpp.conf");
 		if(conffile)
@@ -95,12 +95,12 @@ int config()
 		conffile.close();
 		if(conf_mode > 1 || conf_mode < 0 || conf_log > 1 || conf_log < 0 || conf_high < 0) // проверка полученных значений
 		{
-			std::cerr <<   " Invalid config found!\n"
-						<< " Check it:\n"
-						<< " - 2 field - mining mode: 0 or 1 only\n"
-						<< " - 3 field - logging mode: 0 or 1 only\n"
-						<< " - 5 field - string to search by name: a-z, 0-9 and ':' symbols only\n"
-						<< " Remove or correct sygcpp.conf and run SYG again."<< std::endl;
+			std::cerr << " Invalid config found!\n"
+			          << " Check it:\n"
+			          << " - 2 field - mining mode: 0 or 1 only\n"
+			          << " - 3 field - logging mode: 0 or 1 only\n"
+			          << " - 5 field - string to search by name: a-z, 0-9 and ':' symbols only\n"
+			          << " Remove or correct sygcpp.conf and run SYG again."<< std::endl;
 			return -2;
 		}
 
@@ -135,17 +135,17 @@ void testoutput()
 			log_file = "syg-byname.txt";
 
 		std::ifstream test(log_file);
-		if(!test)	// проверка наличия выходного файла
+		if(!test) // проверка наличия выходного файла
 		{
 			test.close();
 			std::ofstream output(log_file);
 			output <<  "**************************************************************************\n"
-					<< "Change EncryptionPublicKey and EncryptionPrivateKey to your yggdrasil.conf\n"
-					<< "Windows: C:\\ProgramData\\Yggdrasil\\yggdrasil.conf\n"
-					<< "Debian: /etc/yggdrasil.conf\n\n"
-					<< "Visit HowTo.Ygg wiki for more information (russian language page):\n"
-					<< "http://[300:529f:150c:eafe::6]/doku.php?id=yggdrasil:simpleygggen_cpp\n"
-					<< "**************************************************************************\n";
+			       << "Change EncryptionPublicKey and EncryptionPrivateKey to your yggdrasil.conf\n"
+			       << "Windows: C:\\ProgramData\\Yggdrasil\\yggdrasil.conf\n"
+			       << "Debian: /etc/yggdrasil.conf\n\n"
+			       << "Visit HowTo.Ygg wiki for more information (russian language page):\n"
+			       << "http://[300:529f:150c:eafe::6]/doku.php?id=yggdrasil:simpleygggen_cpp\n"
+			       << "**************************************************************************\n";
 			output.close();
 		} else test.close();
 	}
@@ -153,8 +153,8 @@ void testoutput()
 
 struct BoxKeys
 {
-    uint8_t PublicKey[KEYSIZE];
-    uint8_t PrivateKey[KEYSIZE];
+	uint8_t PublicKey[KEYSIZE];
+	uint8_t PrivateKey[KEYSIZE];
 };
 
 BoxKeys getKeyPair()
@@ -181,17 +181,17 @@ BoxKeys getKeyPair()
 int getOnes(const unsigned char HashValue[SHA512_DIGEST_LENGTH])
 {
 	bool done = false;
-	int lOnes = 0;	// кол-во лидирующих единиц
+	int lOnes = 0; // кол-во лидирующих единиц
 
-	std::vector<std::bitset<8>> bytes;	// вектор с однобайтовыми битсетами (двумерный массив)
-	for(int i = 0; i < 32; ++i)			// всего 32 байта, т.к. лидирующих единиц больше быть не может (32*8 = 256 бит, а ff = 255)
-		bytes.push_back(HashValue[i]);	// вставка в вектор с битсетами одного i-того байта хэша
+	std::vector<std::bitset<8>> bytes; // вектор с однобайтовыми битсетами (двумерный массив)
+	for(int i = 0; i < 32; ++i)        // всего 32 байта, т.к. лидирующих единиц больше быть не может (32*8 = 256 бит, а ff = 255)
+		bytes.push_back(HashValue[i]); // вставка в вектор с битсетами одного i-того байта хэша
 
 	for(auto vector_count = bytes.begin(); vector_count != bytes.end() && !done; vector_count++)
 	{
 		for(int i = 7; i >= 0 && !done; --i)
 		{
-			if((*vector_count)[i] == 1)	// обращение к i-тому элементу битсета
+			if((*vector_count)[i] == 1) // обращение к i-тому элементу битсета
 				++lOnes;
 			if((*vector_count)[i] == 0)
 				done = true;
@@ -202,17 +202,17 @@ int getOnes(const unsigned char HashValue[SHA512_DIGEST_LENGTH])
 
 std::string getAddress(unsigned char HashValue[SHA512_DIGEST_LENGTH])
 {
-	// функция "портит" массив хэша, т.к. копирование массива не происходит
-	int lErase = getOnes(HashValue) + 1;	// лидирующие единицы и первый ноль
+	// функция "портит" массив хеша, т.к. копирование массива не происходит
+	int lErase = getOnes(HashValue) + 1; // лидирующие единицы и первый ноль
 
 	bool changeit = false;
 	int bigbyte = 0;
 
-	for(int j = 0; j < lErase; ++j)	// побитовое смещение
+	for(int j = 0; j < lErase; ++j) // побитовое смещение
 	{
 		for(int i = 63; i >= 0; --i)
 		{
-			if(bigbyte == i+1)	// предыдущий байт требует переноса
+			if(bigbyte == i+1) // предыдущий байт требует переноса
 				changeit = true;
 
 			if(HashValue[i] & 0x80)
@@ -236,33 +236,33 @@ std::string getAddress(unsigned char HashValue[SHA512_DIGEST_LENGTH])
 
 	for(int i = 0; i < 14; ++i)
 	{
-	    if(i % 2 == 0)	// если работаем с первым байтом секции
-	    {
-	    	if(HashValue[i] == 0)	// если байт нулевой
-	    	{
-	    		if(HashValue[i+1] == 0) // если следующий байт нулевой
-	    		{
-	    			if(HashValue[i+2] == 0 && i+2 < 13 && HashValue[i+3] == 0 && i+3 <= 13 && !shortadd)
-	    			{
-	    				ss << ":";
-	    				i += 3;
-	    				shortadd = true;
-	    				continue;
-	    			} else {
-	    				ss << "0";
-	    				++i;
-	    			}
-	    		}
-	    	} else {
-	    		ss << std::hex << (int)HashValue[i];
-	    	}
-	    } else {	// если работаем со вторым байтом секции
-	    	if(HashValue[i-1] == 0)	// если предыдущий первый байт был нулевой, нули сокращаем
-	    		ss << std::hex << (int)HashValue[i];
-	    	else
-	    		ss << std::setw(2) << std::setfill('0') << std::hex << (int)HashValue[i];
-	    }
-		if(i != 13 && i % 2 != 0)	// не выводим двоеточие в конце адреса и после первого байта секции
+		if(i % 2 == 0) // если работаем с первым байтом секции
+		{
+			if(HashValue[i] == 0) // если байт нулевой
+			{
+				if(HashValue[i+1] == 0) // если следующий байт нулевой
+				{
+					if(HashValue[i+2] == 0 && i+2 < 13 && HashValue[i+3] == 0 && i+3 <= 13 && !shortadd)
+					{
+						ss << ":";
+						i += 3;
+						shortadd = true;
+						continue;
+					} else {
+						ss << "0";
+						++i;
+					}
+				}
+			} else {
+				ss << std::hex << (int)HashValue[i];
+			}
+		} else { // если работаем со вторым байтом секции
+			if(HashValue[i-1] == 0) // если предыдущий первый байт был нулевой, нули сокращаем
+				ss << std::hex << (int)HashValue[i];
+			else
+				ss << std::setw(2) << std::setfill('0') << std::hex << (int)HashValue[i];
+		}
+		if(i != 13 && i % 2 != 0) // не выводим двоеточие в конце адреса и после первого байта секции
 			ss << ":";
 	}
 	return ss.str();
@@ -282,7 +282,7 @@ void getConsoleLog()
 		std::time_t realtime = std::time(NULL);
 
 		std::cout << " # count [ " << std::dec << std::setfill('.') << std::setw(19) << totalcount << " ] [ "
-				<< std::setw(15) << totalcountfortune << " ] " << std::asctime(std::localtime(&realtime));
+		          << std::setw(15) << totalcountfortune << " ] " << std::asctime(std::localtime(&realtime));
 		std::cout.flush();
 	}
 	mtx.unlock();
@@ -301,7 +301,7 @@ void highminer()
 		SHA512(myKeys.PublicKey, KEYSIZE, HashValue);
 		int newones = getOnes(HashValue);
 
-		if(newones > conf_high)	// сохранение лучших ключей
+		if(newones > conf_high) // сохранение лучших ключей
 		{
 			conf_high = newones;
 			for(int i = 0; i < KEYSIZE; ++i)
@@ -367,7 +367,7 @@ void nameminer()
 		SHA512(myKeys.PublicKey, KEYSIZE, HashValue);
 		std::string tempstr = getAddress(HashValue);
 
-		if(tempstr.find(conf_search.c_str()) != std::string::npos)	// сохранение найденных ключей
+		if(tempstr.find(conf_search.c_str()) != std::string::npos) // сохранение найденных ключей
 		{
 			for(int i = 0; i < KEYSIZE; ++i)
 			{
@@ -415,13 +415,14 @@ void nameminer()
 		getConsoleLog();
 	}
 }
+
 // ------------------------------------------------------
 int main()
 {
 	intro();
 
 	int configcheck = config();
-	if(configcheck < 0)	// функция получения конфигурации
+	if(configcheck < 0) // функция получения конфигурации
 	{
 		std::cerr << "Error code: " << configcheck << std::endl;
 		system("PAUSE");
@@ -429,7 +430,7 @@ int main()
 	}
 
 	testoutput();
-	if(conf_mode == 1)	// запуск соответствующего режима майнинга
+	if(conf_mode == 1) // запуск соответствующего режима майнинга
 	{
 		std::thread * threads[conf_proc];
 		for(int i = 0; i < conf_proc; ++i)
@@ -438,7 +439,7 @@ int main()
 		for(int i = 0; i < conf_proc - 1; ++i)
 			threads[i]->detach();
 
-		threads[conf_proc-1]->join();	// "ждем" последний трэд, бесконечное ожидание
+		threads[conf_proc-1]->join(); // "ждем" последний трэд, бесконечное ожидание
 	}
 	else
 	{
